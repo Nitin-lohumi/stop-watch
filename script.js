eda51123f023f9789b9d3ba68b39110df830b011
Nitin Lohumi <140873160+Nitin-lohumi@users.noreply.github.com>
2023-11-04 04:38:14 +0530

    INDEX.html

---
+++ script.js
@@ -0,0 +1,80 @@
+let hr =0;
+let min =0;
+let  sec =0;
+let count =0;
+
+let timer = false;
+
+
+
+function start(){
+  timer =true;
+  stopWatch();
+
+}
+function stop(){
+    timer =false;
+    
+}
+function reset(){
+    timer =false;
+     hr =0;
+    min =0;
+    sec =0;
+    count =0;
+    document.getElementById("hr").innerHTML=hr+"0";
+    document.getElementById("min").innerHTML=min+"0";
+    document.getElementById("sec").innerHTML=sec+"0";
+    document.getElementById("count").innerHTML=count+"0";
+}
+
+function stopWatch(){
+   if(timer==true){
+    count=count +1;
+    if(count==100){
+        sec =sec +1;
+        count =0;
+    }
+    if(sec ==60){
+        min = min +1;
+        sec =0;
+
+    }
+    if(min == 60){
+        hr =hr +1;
+        min =0;
+        sec =0;
+    }
+
+    let hrString=hr;
+    let minString=min;
+    let secString=sec;
+    let countString=count;
+    if(hr<10){
+       hrString="0"+hr;
+
+    }
+    if(min<10){
+        hrString="0"+hr;
+ 
+     }
+     if(min<10){
+        minString="0"+min;
+ 
+     }
+     if(sec<10){
+        secString="0"+sec;
+ 
+     }
+
+    if(count<10){
+        countString="0"+count;
+    }
+
+    document.getElementById("hr").innerHTML=hrString;
+    document.getElementById("min").innerHTML=minString;
+    document.getElementById("sec").innerHTML=secString;
+    document.getElementById("count").innerHTML=countString;
+    setTimeout('stopWatch()',10);
+   }
+}
